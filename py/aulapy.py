48e440b0e7673250d81430dac4306682fc975802
Helber Oliveira <mellodica.louise@gmail.com>
2021-08-19 22:43:37 -0300

    Teste List comprehensions

---
+++ py/aulapy.py
@@ -0,0 +1,18 @@
+numeros = [1, 2, 3, 4,]
+novos_numeros = numeros
+
+print(novos_numeros)
+
+numeros[0] = 10
+
+print(novos_numeros)
+
+novos_numericos = [numero for numero in numeros]
+
+print(novos_numericos)
+
+
+
+
+
+
